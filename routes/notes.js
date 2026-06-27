b7848399daf793a796f99118e93b222ba830952f
ai-cyber-uc <2301335026do@cyber-u.ac.jp>
2026-06-27 13:27:42 +0900

    Add MongoDB connection to notes route

---
+++ routes/notes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+// ★ご自分のURI(今はダミー)
+const uri = "mongodb+srv://dummy_user:dummy_password@cluster0.xxxxxx.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+  // データベース、コレクションを指定
+  const database = client.db('notes');
+  const notes = database.collection('notes');
+
+  // idが2のドキュメントを取得
+  const query = { id: 2 };
+  const note = await notes.findOne(query);
+
+  // ブラウザにJSON形式でレスポンスを返す
+  res.json(note);
+});
+
+module.exports = router;
